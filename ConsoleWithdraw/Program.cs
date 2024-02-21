aafbc812473128212d018910f2893c98fa2a9f8b
RamonAPereira <ramonpereiranzd@gmail.com>
2024-02-20 22:07:18 -0300

    source

---
+++ ConsoleWithdraw/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using ConsoleWithdraw.Entities;
+using ConsoleWithdraw.Entities.Exceptions;
+
+Console.WriteLine("Enter account data:");
+Console.Write("Number: ");
+int accountNumber = int.Parse(Console.ReadLine());
+Console.Write("Holder: ");
+string AccountHolder = Console.ReadLine();
+Console.Write("Initial balance: ");
+double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Withdraw limit: ");
+double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Account account = new Account(accountNumber, AccountHolder, initialBalance, withdrawLimit);
+
+Console.Write("Enter amount for withdraw: ");
+double withdrawAmount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+try
+{
+    Console.WriteLine(account.Withdraw(withdrawAmount).ToString("F2"));
+}
+catch(DomainException exception)
+{
+    Console.WriteLine(exception.Message);
+}
